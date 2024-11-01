03e30b5ebe21b713d9ad7f88466a5ecb1b956c03
Brandon0304 <bortiz_194@cue.edu.co>
2024-10-31 21:34:48 -0500

    Supermercado con hilos y base de datos

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,47 @@
+package main;
+
+import procesos.Supermercado;
+import utilidades.BaseDatos;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        BaseDatos baseDatos = new BaseDatos();
+        Supermercado supermercado = new Supermercado(baseDatos);
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
+
+        do {
+            System.out.println("TIENDAS D1");
+            System.out.println("1. Iniciar Simulación");
+            System.out.println("2. Mostrar Resultados");
+            System.out.println("3. Borrar Datos");
+            System.out.println("4. Salir");
+            System.out.print("Seleccione una opción: ");
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    supermercado.inicializarClientes(10);
+                    supermercado.iniciarSimulacion();
+                    break;
+                case 2:
+                    baseDatos.mostrarResultados();
+                    break;
+                case 3:
+                    baseDatos.borrarDatos();
+                    System.out.println("Los datos fueron borrados correctamente.");
+                    break;
+                case 4:
+                    System.out.println("Saliendo del programa...");
+                    break;
+                default:
+                    System.out.println("Opción no válida.");
+                    break;
+            }
+        } while (opcion != 4);
+
+        scanner.close();
+    }
+}
